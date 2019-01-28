9a92119cf8fd8c5c6676df97aef02be1f32ea4a4
labohkip81 <labankiplagat81@gmail.com>
2019-01-28 18:56:50 +0300

    added hello world as an object.
    Signed-off-by: labohkip81 <labankiplagat81@gmail.com>

---
+++ src/1.kt
@@ -0,0 +1,7 @@
+//this defines the package used to compile the kotlin app
+package laban.program
+
+fun main(args: Array<String>){
+    println("Hello there")
+}
+
---
+++ src/Hello_as_a_class.kt
@@ -0,0 +1,9 @@
+package laban.program
+//this takes hello world as an object
+class App {
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            println("Hello World")
+        }
+    }
+}
