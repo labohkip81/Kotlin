b0fe8723736b0e2d5ec9cc573f2bcf7e719ce4c9
labohkip81 <labankiplagat81@gmail.com>
2019-01-28 19:12:35 +0300

    Added a function and passed parameters using Kotlin
    Signed-off-by: labohkip81 <labankiplagat81@gmail.com>

--- src/Hello_as_a_class.kt
+++ src/Hello_as_a_class.kt
@@ -1,9 +1,20 @@
 package laban.program
+import java.util.*
 //this takes hello world as an object
 class App {
     companion object {
         @JvmStatic fun main(args: Array<String>) {
             println("Hello World")
+            println(sum(1,20))
         }
     }
+}
+
+//using a function in kotlin
+fun sum(a:Int,b:Int):Int{
+    return a+b
+}
+//This program calculates the average for a test taken by students
+fun Test_average(Math:Int,Kis:Int,Eng:Int):Int{
+    return (Math+Kis+Eng)/3
 }
